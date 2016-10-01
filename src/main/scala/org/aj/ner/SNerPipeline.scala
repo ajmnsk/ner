3769ba7903411dada1ac3bdc11168151ec521d15
ajmnsk <ajmnsk@gmail.com>
2016-10-01 07:41:37 -0400

    Updated comments

--- src/main/scala/org/aj/ner/SNerPipeline.scala
+++ src/main/scala/org/aj/ner/SNerPipeline.scala
@@ -10,12 +10,12 @@ object SNerPipeline {
   /**
     * Creates StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and on.
     * Default CoreNLP / NER models are used If no parameter(s) passed.
-    * You can re-palce default models by providing your own list of models.
+    * You can replace default models by providing your own list of models.
     * Default models can be a part of your list, just need to indicate their location, for example:
     *   ./lib/english.all.3class.distsim.crf.ser.gz,./models/my-model.ser.gz
     *
     * @param regexnerPath path to a file with tokens. Can be used to improve coverage, override default models results.
-    * @param nerPath path to a comma delimited list of models, if passed, it will be used instead of default CoreNLP / NER models
+    * @param nerPath path to a comma delimited list of models, if passed, it will be used instead of default CoreNLP models
     * @return
     */
   def apply(regexnerPath: Option[String] = None, nerPath: Option[String] = None): Pipeline = {
